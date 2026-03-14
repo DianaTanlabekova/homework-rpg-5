dd3b07e052064585438d88693f607f3a7d12f519
DianaTanlabekova <diana.tanlabekova@narxoz.kz>
2026-03-14 20:21:22 +0500

    Process battle completion rewards

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -4,8 +4,11 @@ public class RewardService {
     public String determineReward(AdventureResult battleResult) {
         // TODO: Decide reward rules based on battle outcome.
         if (battleResult == null) {
-            return "TODO";
+            return "No reward";
         }
-        return "TODO";
+        if ("Hero".equals(battleResult.getWinner())) {
+            return "150 gold and 40 XP";
+        }
+         return "No reward";
     }
 }
